3e4cb57dc341c2caa092064d0c0b41eb01bd0608
theghost1980 <saturnob612@gmail.com>
2025-04-09 23:14:48 -0400

    rpl cron by node-cron

--- src/cron/main-cron-job.ts
+++ src/cron/main-cron-job.ts
@@ -1,70 +1,77 @@
-import { CronJob } from "cron";
 import moment from "moment";
+import cron from "node-cron";
 import { ControlVarsUtils } from "../utils/control-vars";
 import { FileUtils } from "../utils/file.utils";
 import { JsonUtils } from "../utils/jsonUtils";
 import { LiquidityPoolUtils } from "../utils/liquidity-pool.utils";
 import { Logger } from "../utils/logger.utils";
 
-const job = new CronJob("0 0 * * *", async () => {
-  //"* * * * *" each minute
-  //"0 0 * * *" each 24h at midnight.
-  //Note: snapshots_24h_days_taken will always match the # of json files within a tokenPair folder
+const job = cron.schedule(
+  "0 0 * * *",
+  async () => {
+    Logger.info("CRON ejecutándose cada 24h - probando: IP");
 
-  Logger.info("CRON Running each 24h -  testing: IP");
-  LiquidityPoolUtils.fetchPoolData()
-    .then((liquidityPoolList) => {
-      if (liquidityPoolList) {
-        for (let i = 0; i < liquidityPoolList.length; i++) {
-          const element = liquidityPoolList[i];
+    try {
+      const liquidityPoolList = await LiquidityPoolUtils.fetchPoolData();
+
+      if (liquidityPoolList && liquidityPoolList.length > 0) {
+        for (const element of liquidityPoolList) {
           const currentTimestampInSeconds = moment().unix();
           const isoDate = moment(
             currentTimestampInSeconds * 1000
           ).toISOString();
-          FileUtils.writeDataToFile(
+          await FileUtils.writeDataToFile(
             `ts_${currentTimestampInSeconds}.json`,
             element.tokenPair,
             { ...element, isoDate }
           );
         }
 
-        JsonUtils.readJsonFile("/reference-data/server-data.json") //inc day count
-          .then((v) => {
-            if (v && v.snapshots_24h_days_taken) {
-              let count = v.snapshots_24h_days_taken;
-              count++;
-              JsonUtils.writeJsonFile("/reference-data/server-data.json", {
-                ...v,
-                snapshots_24h_days_taken: count,
-              });
-            }
-          })
-          .catch((e) =>
-            Logger.error(`Error reading Json file server data! ${e.message}`)
+        try {
+          const serverData = await JsonUtils.readJsonFile(
+            "/reference-data/server-data.json"
           );
+          if (serverData && serverData.snapshots_24h_days_taken !== undefined) {
+            serverData.snapshots_24h_days_taken += 1;
+            await JsonUtils.writeJsonFile(
+              "/reference-data/server-data.json",
+              serverData
+            );
+          }
+        } catch (error: any) {
+          Logger.error(
+            `Error al leer el archivo JSON de datos del servidor: ${error.message}`
+          );
+        }
 
-        Logger.info(`Saved ${liquidityPoolList.length} records YAY!`);
+        Logger.info(`¡Guardados ${liquidityPoolList.length} registros!`);
       } else {
-        Logger.error("No Pools Fetched! Please Check!");
+        Logger.error("¡No se recuperaron pools! Por favor, verifica.");
       }
-    })
-    .catch((e) => {
-      Logger.error(`Error fecthing Liquidity Pools code: ${e.code} `);
-    });
-  Logger.info(
-    `Days Counted: ${ControlVarsUtils.SERVERCOUNT.daysCount.toString()}`
-  );
-});
+    } catch (error: any) {
+      Logger.error(`Error al obtener los pools de liquidez: ${error.code}`);
+    }
+
+    Logger.info(
+      `Días contados: ${ControlVarsUtils.SERVERCOUNT.daysCount.toString()}`
+    );
+  },
+  {
+    scheduled: false,
+  }
+);
 
 const startJob = () => job.start();
-
 const stopJob = () => job.stop();
-
-const getLastDate = () => job.lastDate();
-
-const getLastExecution = () => job.lastDate();
-
-const getNextDate = () => job.nextDate();
+const getNextDate = () => {
+  // TODO bellow using cron-parser
+  // node-cron no proporciona directamente la próxima fecha de ejecución
+  // Se puede calcular manualmente si es necesario
+  Logger.warn(
+    "node-cron no soporta obtener la próxima fecha de ejecución directamente."
+  );
+  return null;
+};
 
 export const MainCronJob = {
   startJob,
